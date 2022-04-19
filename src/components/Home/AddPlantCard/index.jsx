b991639da2541fab40eb33d68a9b5a08324c8c35
abdulvokhid1 <ibrakhimov7747a@gmail.com>
2022-04-19 19:00:07 +0900

    changed

--- src/components/Home/AddPlantCard/index.jsx
+++ src/components/Home/AddPlantCard/index.jsx
@@ -13,7 +13,14 @@ import {
   Featured,
   Featured_Wrapper,
   Flex_Wrapper,
+  Flower_Four,
+  Flower_One,
+  Flower_Three,
+  Flower_Two,
   Icon_Wrapper,
+  Img_Container,
+  Img_Wrapper_One,
+  Img_Wrapper_Two,
   Input_Name,
   Input_Type,
   Input_Wrapper,
@@ -34,8 +41,46 @@ import {
   Type_Plant,
   Type_Wrapper,
   Typing,
+  Flower_Five,
+  Flower_Six,
+  Flower_Seven,
+  Flower_Eight,
+  OnSave,
+  Icon_Card_Wrapper,
+  Garden_Wrapper,
+  Plant_Wrapper,
+  Watering_Wrapper,
+  Email_Wrapper,
+  Garder_Flower,
+  Plant_Flower,
+  Watering_Flower,
+  Word_Wrapper,
+  Garden_Word,
+  Garden_Small,
+  Plant_Word,
+  Plant_Small,
+  Watering_Word,
+  Watering_Small,
+  Join,
+  Inputing,
+  Button_Wrapper,
+  Email_Small,
+  GreenShop,
+  Logo,
+  West_Wrapper,
+  West,
+  Location,
 } from "./style";
 import redstar from "../../../assets/icons/redstar.png";
+import flower1 from "../../../assets/imgs/flower1.png";
+import flower2 from "../../../assets/imgs/flower2.png";
+import flower3 from "../../../assets/imgs/flower3.png";
+import flower4 from "../../../assets/imgs/flower4.png";
+import flower5 from "../../../assets/imgs/flower5.png";
+import flower6 from "../../../assets/imgs/flower6.png";
+import flower7 from "../../../assets/imgs/flower7.png";
+import flower8 from "../../../assets/imgs/flower8.png";
+import { Button } from "antd";
 export const AddPlantCard = () => {
   return (
     <Add_Wrapper>
@@ -93,7 +138,67 @@ export const AddPlantCard = () => {
           <Featured>Featured Image</Featured>
           <RedIcon src={redstar} />
         </Featured_Wrapper>
+        <Img_Container>
+          <Img_Wrapper_One>
+            <Flower_One src={flower1} />
+            <Flower_Two src={flower2} />
+            <Flower_Three src={flower3} />
+            <Flower_Four src={flower4} />
+          </Img_Wrapper_One>
+          <Img_Wrapper_Two>
+            <Flower_Five src={flower5} />
+            <Flower_Six src={flower6} />
+            <Flower_Seven src={flower7} />
+            <Flower_Eight src={flower8} />
+          </Img_Wrapper_Two>
+        </Img_Container>
+        <OnSave>Save Change</OnSave>
       </Type_Wrapper>
+      <Icon_Card_Wrapper>
+        <Garden_Wrapper>
+          <Garder_Flower src={flower1} />
+          <Garden_Word>Garden Care</Garden_Word>
+          <Garden_Small>
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </Garden_Small>
+        </Garden_Wrapper>
+        <Plant_Wrapper>
+          <Plant_Flower src={flower2} />
+          <Plant_Word>Plant Renovation</Plant_Word>
+          <Plant_Small>
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </Plant_Small>
+        </Plant_Wrapper>
+        <Watering_Wrapper>
+          <Watering_Flower src={flower3} />
+          <Watering_Word>Watering Garden</Watering_Word>{" "}
+          <Watering_Small>
+            We are an online plant shop offering a wide range of cheap and
+            trendy plants.
+          </Watering_Small>
+        </Watering_Wrapper>
+        <Email_Wrapper>
+          <Word_Wrapper>Would you like to join newsletters</Word_Wrapper>
+          <Join>
+            <Inputing type="text" placeholder="enter your email address..." />
+            <Button_Wrapper>Join</Button_Wrapper>
+          </Join>
+          <Email_Small>
+            We usually post offers and challenges in newsletter. We’re your
+            online houseplant destination. We offer a wide range of houseplants
+            and accessories shipped directly from our (green)house to yours!
+          </Email_Small>
+        </Email_Wrapper>
+      </Icon_Card_Wrapper>
+      <GreenShop>
+        <Logo />
+        <West_Wrapper>
+          <Location />
+          <West>70 West Buckingham Ave. Farmingdale, NY 11735</West>
+        </West_Wrapper>
+      </GreenShop>
     </Add_Wrapper>
   );
 };
--- src/components/Home/AddPlantCard/style.js
+++ src/components/Home/AddPlantCard/style.js
@@ -1,10 +1,11 @@
 import styled from "styled-components";
-
+import { ReactComponent as logo } from "../../../assets/icons/logo.svg";
+import { ReactComponent as location } from "../../../assets/icons/location.svg";
 const Add_Wrapper = styled.div`
   width: 100%;
   height: 1700px;
   border: 1px solid red;
-  background: grey;
+  /* background: grey; */
   display: flex;
   flex-direction: column;
 `;
@@ -14,7 +15,7 @@ const Type_Wrapper = styled.div`
   margin-left: 289px;
   margin-right: 289px;
   border: 1px solid black;
-  height: 1500px;
+  height: 1000px;
 `;
 const Plant = styled.p`
   font-family: "Inter";
@@ -211,7 +212,259 @@ const Featured = styled.p`
   line-height: 15px;
   color: #3d3d3d;
 `;
+const Img_Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+`;
+const Img_Wrapper_One = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const Img_Wrapper_Two = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+const Flower_One = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Two = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Three = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Four = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Five = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Six = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Seven = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Flower_Eight = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const OnSave = styled.button`
+  background: #46a358;
+  border-radius: 3px;
+  width: 130px;
+  height: 40px;
+  color: white;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffffff;
+  border: none;
+  margin-top: 50px;
+`;
+const Icon_Card_Wrapper = styled.div`
+  display: flex;
+  border: 1px solid black;
+  margin-left: 120px;
+  margin-right: 120px;
+  height: 250px;
+  margin-top: 140px;
+  justify-content: space-around;
+`;
+const Garden_Wrapper = styled.div`
+  border-right: 1px solid rgba(70, 163, 88, 0.1);
+  /* align-items: center; */
+`;
+const Garder_Flower = styled.img`
+  width: 120px;
+  height: 120px;
+`;
+const Garden_Word = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+const Garden_Small = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #727272;
+  width: 200px;
+`;
+const Plant_Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid rgba(70, 163, 88, 0.1);
+`;
+const Plant_Flower = styled.img`
+  width: 120px;
+  height: 120px;
+`;
+const Plant_Word = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+const Plant_Small = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #727272;
+  width: 200px;
+`;
+const Watering_Wrapper = styled.div``;
+const Watering_Flower = styled.img`
+  width: 120px;
+  height: 120px;
+`;
+const Watering_Word = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 17px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+const Watering_Small = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #727272;
+  width: 200px;
+`;
+const Email_Wrapper = styled.div`
+  margin-top: 20px;
+`;
+const Word_Wrapper = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 16px;
+  color: #3d3d3d;
+`;
+const Join = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+`;
+const Inputing = styled.input`
+  background: #ffffff;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.06);
+  border-radius: 6px;
+  outline: none;
+  border: none;
+  height: 40px;
+  width: 250px;
+`;
+const Button_Wrapper = styled.button`
+  background: #46a358;
+  border-radius: 0px 6px 6px 0px;
+  width: 85px;
+  border: none;
+  height: 40px;
+  border: none;
+`;
+const Email_Small = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 22px;
+  /* or 169% */
+
+  color: #727272;
+  width: 354px;
+  height: 89px;
+  margin-top: 30px;
+`;
+const GreenShop = styled.div`
+  background: rgba(70, 163, 88, 0.1);
+  margin-left: 120px;
+  margin-right: 120px;
+  display: flex;
+  height: 70px;
+  text-align: center;
+  justify-content: space-around;
+`;
+const Logo = styled(logo)`
+  text-align: center;
+`;
+const West_Wrapper = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+  text-align: center;
+`;
+const West = styled.p`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: #3d3d3d;
+  width: 185px;
+  height: 44px;
+`;
+const Location = styled(location)``;
 export {
+  West_Wrapper,
+  Location,
+  West,
+  Logo,
+  GreenShop,
+  Email_Small,
+  Join,
+  Inputing,
+  Button_Wrapper,
+  Plant_Small,
+  Plant_Word,
+  Watering_Small,
+  Watering_Word,
+  Garden_Word,
+  Garden_Small,
+  Word_Wrapper,
+  Plant_Flower,
+  Watering_Flower,
+  Garder_Flower,
+  Garden_Wrapper,
+  Plant_Wrapper,
+  Watering_Wrapper,
+  Email_Wrapper,
+  Icon_Card_Wrapper,
+  OnSave,
+  Flower_One,
+  Flower_Two,
+  Flower_Three,
+  Flower_Four,
+  Flower_Five,
+  Flower_Six,
+  Flower_Seven,
+  Flower_Eight,
+  Img_Wrapper_One,
+  Img_Wrapper_Two,
+  Img_Container,
   Featured,
   Featured_Wrapper,
   Desc_Input_Wrapper,
